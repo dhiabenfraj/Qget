a60ccf94bdfa4f941d6b483c6be49549aeb76dac
xreaad <ryadhchalbi0@gmail.com>
2020-09-27 11:10:46 -0500

    clean version :zap:  0.1

--- requirement.py
+++ requirement.py
@@ -5,7 +5,7 @@ import os
 try:
 	os.system('pip3 install colorama')
 	os.system('pip3 install BeautifulSoup4')
-	os.system('pip install requests')
+	os.system('pip3 install requests')
 	os.system('clear')
 except Exception:
 	print('\033[93m [-] please install pip3 - python package \033[00m')
